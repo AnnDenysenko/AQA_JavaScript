3e7074dd67cdd5c6f56726b1d6cc763adb6c3c90
AnnDenysenko <anna.denisenkooo@gmail.com>
2024-07-02 21:39:37 +0300

    Updated Task 5

--- Lesson_5/Task_5_1.mjs
+++ Lesson_5/Task_5_1.mjs
@@ -1,55 +1,40 @@
+// Task 5
+
 // Завдання 1: Визначення рівня успішності студента
 
-// Task 1
-
 import chalk from 'chalk';
-
 import readline from 'readline';
 
-
-// const readline = require('readline');
-
+// Creating an interface to read input from stdin and output to stdout
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
 function runTests(averageGrade) {
-
-if (averageGrade < 60) {
-    console.log(chalk.red("Незадовільно"));
-} else if (averageGrade >= 60 && averageGrade <= 70) {
-    console.log(chalk.yellow("Задовільно"));
-} else if (averageGrade >= 71 && averageGrade <= 80) {
-    console.log(chalk.green("Добре"));
-} else if (averageGrade >= 81 && averageGrade <= 90) {
-    console.log(chalk.blue("Дуже добре"));
-} else if (averageScore >= 91 && averageScore <= 100) {
-    console.log(chalk.magenta("Відмінно"));
-} else {
-    console.log(chalk.gray("Введено некоректне значення"));
+    if (averageGrade < 60) {
+        console.log(chalk.red("Незадовільно"));
+    } else if (averageGrade >= 60 && averageGrade <= 70) {
+        console.log(chalk.yellow("Задовільно"));
+    } else if (averageGrade >= 71 && averageGrade <= 80) {
+        console.log(chalk.green("Добре"));
+    } else if (averageGrade >= 81 && averageGrade <= 90) {
+        console.log(chalk.blue("Дуже добре"));
+    } else if (averageGrade >= 91 && averageGrade <= 100) {
+        console.log(chalk.magenta("Відмінно"));
+    } else {
+        console.log(chalk.gray("Введено некоректне значення"));
+    }
 }
-};
 
+// Asking the user for their grade
 rl.question('Enter your grade: ', (averageGrade) => {
-    // Print the grade to the console
-    console.log(`Your grade is: ${averageGrade}`);
-    // Close the readline interface
+    // Converting the input to a number
+    const grade = Number(averageGrade);
+
+    // Running the test function
+    runTests(grade);
+
+    // Closing the readline interface
     rl.close();
-  });
-  
-
-
-// if (averageGrade < 60) {
-//     console.log(chalk.red("Незадовільно"));
-// } else if (averageGrade >= 60 && averageGrade <= 70) {
-//     console.log(chalk.yellow("Задовільно"));
-// } else if (averageGrade >= 71 && averageGrade <= 80) {
-//     console.log(chalk.green("Добре"));
-// } else if (averageGrade >= 81 && averageGrade <= 90) {
-//     console.log(chalk.blue("Дуже добре"));
-// } else if (averageScore >= 91 && averageScore <= 100) {
-//     console.log(chalk.magenta("Відмінно"));
-// } else {
-//     console.log(chalk.gray("Введено некоректне значення"));
-// }
+});
--- Lesson_5/Task_5_2.mjs
+++ Lesson_5/Task_5_2.mjs
@@ -1,30 +1,47 @@
-// Task 2
+// Task 5
 
-// const chalk = require("chalk");
-// const prompt = require("prompt-sync")();
+// Завдання 2: Виконайте попереднє завдання але використовуючи конструкцію switch/case
 
-// let averageGrade = +prompt("Enter your grade: ");
 
-const averageGrade = 80;
+import chalk from 'chalk';
+import readline from 'readline';
 
-switch (true) {
-    case (averageGrade < 60):
-        console.log(chalk.red("Незадовільно"));
-        break;
-    case (averageGrade >= 60 && averageGrade <= 70):
-        console.log(chalk.yellow("Задовільно"));
-        break;
-    case (averageGrade >= 71 && averageGrade <= 80):
-        console.log(chalk.green("Добре"));
-        break;
-    case (averageGrade >= 81 && averageGrade <= 90):
-        console.log(chalk.blue("Дуже добре"));
-        break;
-    case (averageGrade >= 91 && averageGrade <= 100):
-        console.log(chalk.magenta("Відмінно"));
-        break;
-    default:
-        console.log(chalk.gray("Введено не коректне значення"));
+// Creating an interface to read input from stdin and output to stdout
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function runTests(averageGrade) {
+    switch (true) {
+            case (averageGrade < 60):
+                console.log(chalk.red("Незадовільно"));
+                break;
+            case (averageGrade >= 60 && averageGrade <= 70):
+                console.log(chalk.yellow("Задовільно"));
+                break;
+            case (averageGrade >= 71 && averageGrade <= 80):
+                console.log(chalk.green("Добре"));
+                break;
+            case (averageGrade >= 81 && averageGrade <= 90):
+                console.log(chalk.blue("Дуже добре"));
+                break;
+            case (averageGrade >= 91 && averageGrade <= 100):
+                console.log(chalk.magenta("Відмінно"));
+                break;
+            default:
+                console.log(chalk.gray("Введено не коректне значення"));
+        }
 }
 
+// Asking the user for their grade
+rl.question('Enter your grade: ', (averageGrade) => {
+    // Converting the input to a number
+    const grade = Number(averageGrade);
 
+    // Running the test function
+    runTests(grade);
+
+    // Closing the readline interface
+    rl.close();
+});
